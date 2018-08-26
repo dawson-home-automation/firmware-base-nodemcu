ab54036aefb6d8266a987bc75d5810bc12f3f229
Dawson Israel <dawfre@amazon.com>
2018-08-26 18:20:36 -0300

    Add target to service register

--- src/network/service_register.h
+++ src/network/service_register.h
@@ -5,11 +5,13 @@ String getMacAddress();
 String getIPAddress();
 String macToStr(const uint8_t*);
 
-void registerDevice(void){
+void registerDevice(char* target){
     char body_c[200];
+    String target_str(target);
 
     String body = "{\"ipAddress\":\"" + WiFi.localIP().toString() + "\",";
-    body += "\"name\":\"" + getMacAddress() + "\"}";
+    body += "\"name\":\"" + getMacAddress() + "\",";
+    body += "\"target\":\"" + target_str + "\"}";
     body.toCharArray(body_c, 200);
 
     publish_message("devices/registry", 1, body_c, false);
--- src/network/wifi.h
+++ src/network/wifi.h
@@ -3,24 +3,24 @@
 #include "service_register.h"
 #include "update_server.h"
 
-void connectWifi(char* network_ssid, char* network_password) {
+void connectWifi(char* network_ssid, char* network_password, char* target) {
   while(WiFi.waitForConnectResult() != WL_CONNECTED){
     WiFi.begin(network_ssid, network_password);
     if(WiFi.waitForConnectResult() == WL_CONNECTED) {
       setupUpdateServer();
-      registerDevice();
+      registerDevice(target);
     } else {
       Serial.println("action=wifi status=error");
     }
   }
 }
 
-void setupWifi(char* network_ssid, char* network_password){
+void setupWifi(char* network_ssid, char* network_password, char* target){
   WiFi.mode(WIFI_STA);
   WiFi.begin(network_ssid, network_password);
   if(WiFi.waitForConnectResult() == WL_CONNECTED) {
     Serial.println("action=wifi status=success");
     setupUpdateServer();
-    registerDevice();
+    registerDevice(target);
   }
 }
